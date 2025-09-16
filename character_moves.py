15d23e2779c230b6adf37c0cd977206a57809357
2DPG-E320-05-PC <chajihun1171@gmail.com>
2025-09-16 10:50:53 +0900

    사각운동, 원운동 함수뼈대 완성

--- character_moves.py
+++ character_moves.py
@@ -2,8 +2,18 @@ from pico2d import *
 
 open_canvas()
 
-while True:
 
+def move_rectengle():
+    pass
+
+
+def move_circle():
+    pass
+
+
+while True:
+    move_rectengle()
+    move_circle()
     pass
 
 close_canvas()
